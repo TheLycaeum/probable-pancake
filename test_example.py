b4742420dd9b9367b54ca7681ee65c6bfab71fad
Noufal Ibrahim <noufal@nibrahim.net.in>
2018-09-17 16:33:47 +0530

    Fixes names

--- test_example.py
+++ test_example.py
@@ -1,6 +1,6 @@
 import example
 
-def test_add():
+def test_sub():
     for i in range(10):
         for j in range(10):
-            assert example.add(i, j) == i - j
+            assert example.sub(i, j) == i - j
