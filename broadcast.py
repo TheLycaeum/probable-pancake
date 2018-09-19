104e0c97cbd9819d868f3a7ec5d21a4864cf96f3
Balagovind <balagovind2094@gmail.com>
2018-09-19 15:59:13 +0530

    Works from terminal for twitter.

--- broadcast.py
+++ broadcast.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
+import dispatcher
+
+
 
 def return_msg_from_cmdline():
     message = sys.argv[1]
     return message
 
 
+if __name__ == '__main__':
+    dispatcher.main(return_msg_from_cmdline())
